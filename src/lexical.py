518ae69b1c0ca0c4352c34fc7bdf5d7e05cc0323
nikonia19 <nikonia19crm@gmail.com>
2024-03-04 14:40:23 +0300

    isimler eklendi

--- src/lexical.py
+++ src/lexical.py
@@ -1,8 +1,10 @@
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.tag import pos_tag
+import spacy
 
 nltk.download('averaged_perceptron_tagger')
+nlp = spacy.load('en_core_web_sm')
 
 class Lexical:
     def __init__(self,text):
@@ -13,3 +15,9 @@ class Lexical:
         tagged_words = pos_tag(words)  # etiketleme, bu özne bu yüklem vs
         verbs = [words for word,tag in tagged_words if tag.startswith('VB')] # kontrol edip vb ile başlayanları verbse at kelimeleri
         return verbs
+
+    def get_pople_names(self):
+        doc = nlp(self.text)
+        name = [entity.text for entity in doc.entities if entity.label_ == "PERSON"] # isimleri getirir
+
+        
